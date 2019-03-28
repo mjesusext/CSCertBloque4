32a6dc0f161cf8774e8768095ac38b3d0c471f05
mjesusext <mjesusext@gmail.com>
2019-03-28 21:04:50 +0100

    Creación modulo 11, intento de escribir fichero. En pruebas

---
+++ Modulo11/EditorTextos.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Modulo11
+{
+    public static class EditorTextos
+    {
+        public static void Run()
+        {
+            bool nextOp = true;
+            int opCode = -1;
+            Console.WriteLine("----- Editor de textos -----");
+
+            do
+            {
+                PromptMenu();
+                do
+                {
+                    Console.Write("Introduzca un número: ");
+                }
+                while (!int.TryParse(Console.ReadLine(), out opCode));
+                
+                switch (opCode)
+                {
+                    case 1:
+                        WriteFileFromPrompt();
+                        break;
+                    case 2:
+                        ReadFileFromPrompt();
+                        break;
+                    case 3:
+                        nextOp = false;
+                        break;
+                    default:
+                        opCode = -1;
+                        break;
+                }
+
+            } while (nextOp);
+        }
+
+        private static void PromptMenu()
+        {
+            Console.WriteLine("Seleccione una opción:");
+            Console.WriteLine("1) Crear fichero texto\n" +
+                              "2) Leer fichero texto\n" +
+                              "3) Finalizar\n");
+        }
+
+        private static void WriteFileFromPrompt()
+        {
+            string fileName;
+            string filePath;
+            FileStream fs;
+            byte[] textRow = new byte[] { };
+            byte[] exitCommand = Encoding.Unicode.GetBytes("EXIT\r\n");
+            bool nextRow = true;
+
+            Console.Write("Introduzca nombre de fichero: ");
+            fileName = Console.ReadLine();
+
+            Console.Write("Introduzca ruta: ");
+            filePath = Console.ReadLine();
+
+            try
+            {
+                using (fs = new FileStream(filePath + "\\" + fileName + ".txt", FileMode.Append, FileAccess.Write))
+                {
+                    while (nextRow)
+                    {
+                        textRow = Encoding.Unicode.GetBytes((Console.ReadLine() + "\r\n"));
+                        
+                        if (textRow.SequenceEqual(exitCommand))
+                        {
+                            fs.Write(textRow, 0, textRow.Length - 1);
+                        }
+                        else
+                        {
+                            nextRow = false;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error creando fichero (o abriendo si existe). Retorno al menu");
+                return;
+            }
+        }
+
+        private static void ReadFileFromPrompt()
+        {
+
+        }
+    }
+}
---
+++ Modulo11/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Modulo11")]
+[assembly: AssemblyDescription("Ensamblado para solución de ejercicios de bloque 4 - modulo 11")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Extreme Solutions")]
+[assembly: AssemblyProduct("Modulo11")]
+[assembly: AssemblyCopyright("Copyright © Extreme Solutions 2019")]
+[assembly: AssemblyTrademark("Extreme Solutions Trademark")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("8f12b310-8055-419b-a5c4-62bf64d47b60")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
