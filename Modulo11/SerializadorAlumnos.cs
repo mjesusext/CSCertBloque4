d611b58ddbaccb2b41daaa2af1dd777bef1330de
mjesusext <mjesusext@gmail.com>
2019-04-02 19:29:06 +0200

    Ejercicios previos al de Reflection hechos y probados. Generalizada la ruta a escritorio para facilitar pruebas. Resta revisar mejor las opciones XML

--- Modulo11/SerializadorAlumnos.cs
+++ Modulo11/SerializadorAlumnos.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Xml.Serialization;
 
@@ -23,11 +24,53 @@ namespace Modulo11
             Close = 6
         };
 
+        private enum OpcionSerializadorModo
+        {
+            Binary = 1,
+            XML = 2,
+            JSON = 3
+        };
+
+        private static readonly string DataPath;
+        private static OpcionSerializadorModo SerMode;
         private static List<AlumnoM11> Alumnos;
 
         static SerializadorAlumnos()
         {
+            string UserName;
             Alumnos = new List<AlumnoM11>();
+            
+            Console.Write("Introduza nombre usuario: ");
+            UserName = Console.ReadLine();
+
+            //Generalización del ejercicio, para permitir hacer pruebas de los diferentes modos de serialización
+            #if DEBUG
+            Console.WriteLine("Seleccione modo de serialización:");
+            Console.WriteLine("1) Binaria\n" +
+                                "2) XML\n" +
+                                "3) JSON\n");
+            do
+            {
+                Console.Write("Modo escogido: ");
+            } while (!Enum.TryParse(Console.ReadLine(), out SerMode));
+            #else
+            SerMode = OpcionSerializadorModo.XML;
+            #endif
+
+            switch (SerMode)
+            {
+                case OpcionSerializadorModo.Binary:
+                    DataPath = @"C:\Users\" + UserName + @"\Desktop\SerializadoAlumnosM11.dat";
+                    break;
+                case OpcionSerializadorModo.XML:
+                    DataPath = @"C:\Users\" + UserName + @"\Desktop\SerializadoAlumnosM11.xml";
+                    break;
+                case OpcionSerializadorModo.JSON:
+                    DataPath = @"C:\Users\" + UserName + @"\Desktop\SerializadoAlumnosM11.txt";
+                    break;
+                default:
+                    break;
+            }
         }
 
         public static void Run()
@@ -95,16 +138,19 @@ namespace Modulo11
 
             Alumnos.Add(al);
             Console.WriteLine("Alumnos en lista: {0}", Alumnos.Count);
+            Console.WriteLine();
         }
 
         public static void ShowStudents()
         {
+            Console.WriteLine();
             Console.WriteLine("Listado de alumnos: ");
 
             foreach (AlumnoM11 al in Alumnos)
             {
                 Console.WriteLine(al.ToString());
             }
+            Console.WriteLine();
         }
 
         public static void FlushStudents()
@@ -114,81 +160,137 @@ namespace Modulo11
 
         public static void SerializeStudents()
         {
-            #region Binary Serialization
-            BinaryFormatter BinFormatter = new BinaryFormatter();
-            using (FileStream fs = new FileStream(@"C:\Users\mextreme\Desktop\SerializadoAlumnosM11.dat", FileMode.Create))
+            switch (SerMode)
             {
-                try
-                {
-                    BinFormatter.Serialize(fs, Alumnos);
-                }
-                catch (SerializationException e)
-                {
-                    Console.WriteLine("Error en serialización binaria de la colección de alumnos. Mensaje: {0}", e.Message);
-                }
-            }
-            #endregion
+                case OpcionSerializadorModo.Binary:
+                    BinaryFormatter BinFormatter = new BinaryFormatter();
 
-            #region Xml Serialization
-            //Requiere visibilidad de clase a serializar
-            //El serializador ya codifica correctamente el objeto a string (no tenemos que hacer encodings)
-            XmlSerializer Xmlformatter = new XmlSerializer(typeof(List<AlumnoM11>));
-            using (FileStream fs = new FileStream(@"C:\Users\mextreme\Desktop\SerializadoAlumnosM11.xml", FileMode.Create))
-            {
-                try
-                {
-                    Xmlformatter.Serialize(fs, Alumnos);
-                }
-                catch (SerializationException e)
-                {
-                    Console.WriteLine("Error en serialización XML de la colección de alumnos. Mensaje: {0}", e.Message);
-                }
+                    using (FileStream fs = new FileStream(DataPath, FileMode.Create))
+                    {
+                        try
+                        {
+                            BinFormatter.Serialize(fs, Alumnos);
+                        }
+                        catch (SerializationException e)
+                        {
+                            Console.WriteLine("Error en serialización binaria de la colección de alumnos. Mensaje: {0}", e.Message);
+                        }
+                    }
+                    break;
+                case OpcionSerializadorModo.XML:
+                    //Requiere visibilidad de clase a serializar
+                    //El serializador ya codifica correctamente el objeto a string (no tenemos que hacer encodings)
+                    XmlSerializer Xmlformatter = new XmlSerializer(typeof(List<AlumnoM11>));
+
+                    using (FileStream fs = new FileStream(DataPath, FileMode.Create))
+                    {
+                        try
+                        {
+                            Xmlformatter.Serialize(fs, Alumnos);
+                        }
+                        catch (SerializationException e)
+                        {
+                            Console.WriteLine("Error en serialización XML de la colección de alumnos. Mensaje: {0}", e.Message);
+                        }
+                    }
+                    break;
+                case OpcionSerializadorModo.JSON:
+                    //Requiere visibilidad de clase a serializar
+                    //El serializador ya codifica correctamente el objeto a string (no tenemos que hacer encodings)
+                    DataContractJsonSerializer JSONFormatter = new DataContractJsonSerializer(typeof(List<AlumnoM11>));
+
+                    using (FileStream fs = new FileStream(DataPath, FileMode.Create))
+                    {
+                        try
+                        {
+                            JSONFormatter.WriteObject(fs, Alumnos);
+                        }
+                        catch (SerializationException e)
+                        {
+                            Console.WriteLine("Error en serialización JSON de la colección de alumnos. Mensaje: {0}", e.Message);
+                        }
+                    }
+                    break;
+                default:
+                    break;
             }
-            #endregion
         }
 
         public static void DeserializeStudents()
         {
-            #region Binary Deserialization
-            BinaryFormatter BinFormatter = new BinaryFormatter();
-            using (FileStream fs = new FileStream(@"C:\Users\mextreme\Desktop\SerializadoAlumnosM11.dat", FileMode.Open))
+            switch (SerMode)
             {
-                try
-                {
-                    Alumnos = (List<AlumnoM11>)BinFormatter.Deserialize(fs);
-                }
-                catch (SerializationException e)
-                {
-                    Console.WriteLine("Error en deserialización binaria del fichero de alumnos. Mensaje: {0}", e.Message);
-                }
-            }
-            #endregion
+                case OpcionSerializadorModo.Binary:
+                    BinaryFormatter BinFormatter = new BinaryFormatter();
 
-            #region Xml Deserialization
-            //Requiere visibilidad de clase a serializar
-            //El serializador ya codifica correctamente el objeto a string (no tenemos que hacer encodings)
-            XmlSerializer XmlFormatter = new XmlSerializer(typeof(List<AlumnoM11>));
+                    using (FileStream fs = new FileStream(DataPath, FileMode.Open))
+                    {
+                        try
+                        {
+                            Alumnos = (List<AlumnoM11>)BinFormatter.Deserialize(fs);
+                        }
+                        catch (SerializationException e)
+                        {
+                            Console.WriteLine("Error en deserialización binaria del fichero de alumnos. Mensaje: {0}", e.Message);
+                        }
+                    }
+                    break;
+                case OpcionSerializadorModo.XML:
+                    //Requiere visibilidad de clase a serializar
+                    //El serializador ya codifica correctamente el objeto a string (no tenemos que hacer encodings)
+                    XmlSerializer XmlFormatter = new XmlSerializer(typeof(List<AlumnoM11>));
 
-            using (FileStream fs = new FileStream(@"C:\Users\mextreme\Desktop\SerializadoAlumnosM11.xml", FileMode.Open))
-            {
-                try
-                {
-                    Alumnos = (List<AlumnoM11>)XmlFormatter.Deserialize(fs);
-                }
-                catch (SerializationException e)
-                {
-                    Console.WriteLine("Error en deserialización XML del fichero de alumnos. Mensaje: {0}", e.Message);
-                }
+                    using (FileStream fs = new FileStream(DataPath, FileMode.Open))
+                    {
+                        try
+                        {
+                            Alumnos = (List<AlumnoM11>)XmlFormatter.Deserialize(fs);
+                        }
+                        catch (SerializationException e)
+                        {
+                            Console.WriteLine("Error en deserialización XML del fichero de alumnos. Mensaje: {0}", e.Message);
+                        }
+                    }
+                    break;
+                case OpcionSerializadorModo.JSON:
+                    //Requiere visibilidad de clase a serializar
+                    //El serializador ya codifica correctamente el objeto a string (no tenemos que hacer encodings)
+                    DataContractJsonSerializer JSONFormatter = new DataContractJsonSerializer(typeof(List<AlumnoM11>));
+
+                    using (FileStream fs = new FileStream(DataPath, FileMode.Open))
+                    {
+                        try
+                        {
+                            Alumnos = (List<AlumnoM11>)JSONFormatter.ReadObject(fs);
+                        }
+                        catch (SerializationException e)
+                        {
+                            Console.WriteLine("Error en deserialización JSON del fichero de alumnos. Mensaje: {0}", e.Message);
+                        }
+                    }
+                    break;
+                default:
+                    break;
             }
-            #endregion
         }
     }
 
     [Serializable]
     public class AlumnoM11
     {
+        public enum TipoPersona
+        {
+            Alumno = 1,
+            Profesor = 2
+        };
+
         public string Nombre { get; set; }
         public string Apellidos { get; set; }
+
+        [XmlAttribute]
+        public DateTime FechaNacimiento = DateTime.Today;
+        [XmlAttribute]
+        public TipoPersona TipoPers = TipoPersona.Alumno;
 
         public override string ToString()
         {
