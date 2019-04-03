cca2ca0c2a0d41da8047180a2c7f4531407ebd05
Manuel Jesus Extremera Extremera <manuel.extremera@scmspain.com>
2019-04-03 12:47:26 +0200

    Versión codigo desde consola (creando plantilla program - main) probada OK. Resta aclarar viabilidad de añadir usings al vuelo

--- Modulo11/CargadorCodigo.cs
+++ Modulo11/CargadorCodigo.cs
@@ -108,6 +108,85 @@ namespace Modulo11
 
         private static void RunFromConsole()
         {
+            bool NextLine = true;
+            StringBuilder CodeStatements = new StringBuilder();
+            StringBuilder CodeTemplate = new StringBuilder();
+            CSharpCodeProvider cs_provider;
+            CompilerParameters comp_params;
+            CompilerResults comp_results;
+
+            CodeTemplate.Append(
+                @"using System;
+                using System.Collections.Generic;
+                using System.Linq;
+                using System.Text;
+                using System.Threading.Tasks;
+
+                namespace ModuloX
+                {
+                    class Program
+                    {
+                        static void Main(string[] args)
+                        {
+                            {CodeStatements}
+                        }
+                    }
+                }");
+
+            Console.WriteLine("--- Editor de codigo integrado en consola. Escriba EXIT para acabar la edición ----\n");
+            while (NextLine)
+            {
+                string codeLine = Console.ReadLine();
+
+                if (codeLine.ToLower() == "exit")
+                {
+                    NextLine = false;
+                }
+                else
+                {
+                    CodeStatements.AppendLine(codeLine);
+                }
+            }
+
+            CodeTemplate.Replace("{CodeStatements}", CodeStatements.ToString());
+            Console.WriteLine("\n--- Codigo montado en plantilla ---\n");
+
+            //Instanciamos un proveedor de compilación específico de CS. Si quisieramos hacerlo dinámico, trabajaríamos con su clase base CodeDomProvider
+            cs_provider = new CSharpCodeProvider();
+
+            //Definimos parametros del compilador
+            comp_params = new CompilerParameters();
+            comp_params.GenerateInMemory = true;
+            comp_params.GenerateExecutable = false;
+            comp_params.ReferencedAssemblies.Add("System.dll");
+            comp_params.ReferencedAssemblies.Add("System.Linq.dll");
+
+            //Compilamos
+            comp_results = cs_provider.CompileAssemblyFromSource(comp_params, CodeTemplate.ToString());
+
+            if (comp_results.Errors.Count == 0)
+            {
+                Console.WriteLine("\n--- Compilacion de biblioteca en memoria correcta ---\n");
+                Assembly created_assembly = comp_results.CompiledAssembly;
+
+                //Podriamos acceder desde modulo (que solo debe existir 1 puesto que no mezclamos lenguajes) o desde el tipo identificado en el ensamblado
+                //Recuperamos un objeto de la clase principal que contiene el metodo main
+                Type SelType = created_assembly.GetType("ModuloX.Program");
+
+                //Si no se dice lo contrario, solo recupera métodos publicos (sean estáticos o de instancia). Forzamos recuperar todos para ver el Main (que es privado)
+                MethodInfo MainStaticMethod = SelType.GetMethod("Main", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+
+                Console.WriteLine("\n--- Invocando programa compilado en memoria.... ---\n");
+                //El método Invoke va encapsulado del siguiente modo --> Matriz de objetos por cada argumento de función.
+                //Main es de un argumento que es una matriz de strings, por lo tanto hay que anidarle dicho array vacío como primer y único argumento
+                MainStaticMethod.Invoke(SelType, new object[] { new string[] { } });
+
+                Console.WriteLine("\n--- Codigo compilado finalizado ---\n");
+            }
+            else
+            {
+                Console.WriteLine("Compilacion de biblioteca en memoria con errores");
+            }
         }
     }
 }
